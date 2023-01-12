234825ac152025531bb80a4dd47d5ac1dfd2c65a
Sacha Gijsbers <sacha@gijsbers.tel>
2023-01-12 16:01:58 +0100

    small changes in names to make it more general

--- code/load.py
+++ code/load.py
@@ -56,16 +56,16 @@ def load_conns(station_list):
 
             splitline[-1] = splitline[-1].strip()
 
-            station1 = splitline[0]
-            station2 = splitline[1]
+            station = splitline[0]
+            destination = splitline[1]
             distance = int(splitline[2])
             
             for place in station_list:
-                if station1 == place.name:
-                    place.add_conn(station2, distance)
+                if station == place.name:
+                    place.add_conn(destination, distance)
 
-                if station2 == place.name:
-                    place.add_conn(station1, distance)
+                if destination == place.name:
+                    place.add_conn(station, distance)
             
     return station_list
 
