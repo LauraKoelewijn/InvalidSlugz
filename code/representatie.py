6bedee5001924df3bd9c3d5d79fa66bbbb9ec793
meikenena <meikenena@gmail.com>
2023-01-12 14:23:26 +0100

    made a class representation of the stations and trains

--- code/representatie.py
+++ code/representatie.py
@@ -1,17 +1,34 @@
 class Station():
-    def __init__(self) -> None:
+    def __init__(self, name: str) -> None:
+        self.name = name
         self.connect = {}
         self.visited = False
-        self.coor = []
+        self.coord = []
 
     def add_conn(self, location, time):
         self.connect[location] = time
 
-    def add_coor(self, x, y):
-        self.coor = [x, y]
+    def add_coord(self, x, y):
+        self.coord = [x, y]
 
     def been(self):
         self.visited = True
 
     def is_visited(self):
         return self.visited
+
+    def __repr__(self):
+        return f"{self.name} at {self.coord} with {self.connect}." 
+
+class Train():
+    def __init__(self, name):
+        self.name = name
+        self.trajectory = []
+        self.time = 0
+
+    def add_station(self, station: "Station", time: int):
+        self.trajectory.append(station)
+        self.time = self.time + time
+
+    def __repr__(self):
+        return f"{self.name} visits {self.trajectory} in {self.time} minutes."
