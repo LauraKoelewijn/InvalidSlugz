e9d15d68b4faeb2948c04112711e6c4053418d43
Sacha Gijsbers <sacha@gijsbers.tel>
2023-01-20 14:43:51 +0100

    basemap visualisation for holland regions

---
+++ map.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import contextily as cx
+
+
+df = gpd.read_file('data/holland_regions.geojson')
+df_wm = df.to_crs(epsg=3857)
+ax = df_wm.plot(figsize=(10, 10), alpha=0.5, edgecolor='k')
+cx.add_basemap(ax)
+
+plt.show()
+
+# # loading file of boarders of Holland
+# df_places = gpd.read_file('data/holland_regions.geojson')
+
+# # looping through regions in the data file and plotting them in a matplotlib graph
+# for polygon in df_places['geometry']:
+#     x,y = polygon.exterior.xy
+#     plt.plot(x,y)
