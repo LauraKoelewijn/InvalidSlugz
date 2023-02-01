5095132d204a163d18d3b626bc2398deb16aacf4
MacBook Pro <macbookpro@MacBook-Pro-van-MacBook.local>
2023-02-01 11:09:33 +0100

    Added code summary and docstring

--- program/analysis_visualisation/visualisation.py
+++ program/analysis_visualisation/visualisation.py
@@ -1,18 +1,33 @@
+#   visualisation.py
+#
+#   Minor programmeren - Algoritmen en Heuristieken
+#   RailNL case
+#   Meike Klunder, Laura Koelewijn & Sacha Gijsbers
+#
+#   Holds the visualize function which can plot all connections of Holland or
+#   NL in black, and then plot the trajectories on top of it in alternating
+#   colours.
+
+# import libraries and classes
 import matplotlib.pyplot as plt # type: ignore
 from typing import List
 
-# from ..representation.network_graph import Network
-
 import matplotlib.transforms as mtrans # type: ignore
 
-def visualize(network, which_region = 'holland') -> None:
+def visualize(network: 'Network', which_region = 'holland') -> None:
+    """Plots all connections in black and then plots trajectories on top of it.
 
+    Args:
+        network ('Network'): a Network object
+        which_region (str): 'holland' or 'nl' depending on which region you
+        want to use.
+    """
     # check if wanted map is of holland of nl and change save option
     if which_region == 'nl':
         save_plot = 'output/nl_plot_con.png'
     elif which_region == 'holland':
         save_plot = 'output/holland_plot_con.png'
-    
+
     # intialize empty list of coordinates for plotting
     coords: List[List[float]] = []
 
@@ -28,7 +43,7 @@ def visualize(network, which_region = 'holland') -> None:
     # set map as background plot of specific size
     ax.imshow(img, extent=[3.1, 7.4, 50.62, 53.73], aspect=1.7)
 
-    # PLOT ALL CONNECTIONS   
+    # PLOT ALL CONNECTIONS
     # loop through all stations in the network_graph
     for station in stations:
         # add station name and coordinates to the lists
@@ -46,8 +61,8 @@ def visualize(network, which_region = 'holland') -> None:
 
             # plot connections between all stations
             # plt.plot(x_line, y_line, color = 'grey', linestyle='--')
-    
-    #hide axes
+
+    # hide axes
     ax = plt.gca()
     ax.get_yaxis().set_visible(False)
     ax.get_xaxis().set_visible(False)
@@ -72,11 +87,10 @@ def visualize(network, which_region = 'holland') -> None:
     # unzip the tuples and plot them
     x,y = zip(*coords)
     plt.scatter(x, y, zorder=10)
-        
-    # # add names of the stations to the points
+
+    # add names of the stations to the points
     # for index, label in enumerate(names):
-    #     plt.annotate(label,(x[index], y[index]))
-        
+
     # save plot
     plt.savefig(save_plot, dpi = 500)
 
